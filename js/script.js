f58b73daeabd451b0b352ad9cae6ea28f267cc6c
echenko <echenko.v.g@yandex.ru>
2023-08-28 20:50:14 +0300

    28/08/23

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function slider(main_slider, counter_slider = 0, index_slider = 0) {
+function slider(main_slider, counter_slider = 0, ) {
     
     function inner() {
         index_slider = (index_slider + counter_slider) % main_slider.length;
+        
         for (i = 0; i < main_slider.length; i++) {
             if (i == index_slider) {
                 main_slider[i].classList.add("active");
@@ -9,21 +10,19 @@ function slider(main_slider, counter_slider = 0, index_slider = 0) {
                 main_slider[i].classList.remove("active");
             }
         }
-        console.log(index_slider);
+        console.log(index_slider, main_slider.length);
     }
     return inner
   
 }
 
 const menu = document.querySelector('.main-container');
-let reason_slider = document.querySelectorAll(".reason-slider-list .reason-slider-image")
+let reason_slider = document.querySelectorAll(".reason-slider-list .reason-slider-item")
 let reason_slider_left = slider(reason_slider, reason_slider.length-1);
 let reason_slider_right = slider(reason_slider, 1);
-
+let index_slider = 0;
 let reason_slider_left_button = document.querySelector(".reason-slider-button.left").onclick = () => reason_slider_left();
 let reason_slider_rigth_button = document.querySelector(".reason-slider-button.right").onclick = () => reason_slider_right();
 
 menu.addEventListener('click', ()=>  document.getElementById("check-menu").checked = false);
-
-slider(reason_slider)
 
